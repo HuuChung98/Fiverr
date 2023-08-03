9f00b83d66f63050abc844112fe5d2e6db08c8fd
HuuChung98 <balechung93@gmail.com>
2023-08-03 22:14:20 +0700

    completed API crete user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,38 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ApiParam, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
 
+class userType {
+  @ApiProperty({ description: "userId", type: Number})
+  nguoi_dung_id: number;
+
+  @ApiProperty({ description: "name", type: String})
+  ten_nguoi_dung: string;
+
+  @ApiProperty({ description: "email", type: String})
+  email: string;
+
+  @ApiProperty({ description: "pass_word", type: String})
+  pass_word: string;
+
+  @ApiProperty({ description: "phone", type: String})
+  phone: string;
+
+  @ApiProperty({ description: "birth_day", type: String})
+  birth_day: string;
+
+  @ApiProperty({ description: "gender", type: String})
+  gender: string;
+
+  @ApiProperty({ description: "role", type: String})
+  role: string;
+
+  @ApiProperty({ description: "skill", type: String})
+  skill: string;
+
+  @ApiProperty({ description: "certification", type: String})
+  certification: string;
+}
+
 @ApiTags("NguoiDung")
 @Controller('api/users')
 export class UserController {
@@ -15,8 +47,8 @@ export class UserController {
   }
 
   @Post()
-  createUser(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  createUser(@Body() values: userType) {
+    return this.userService.createUser(values);
   }
 
   @Delete(':id')
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,62 @@
-import { Injectable } from '@nestjs/common';
+import { Body, HttpException, Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiParam, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
 
 import { PrismaClient, NguoiDung } from '@prisma/client';
+
 
 @Injectable()
 export class UserService {
 
   prisma = new PrismaClient();
 
-  create(createUserDto: CreateUserDto) {
+  async createUser(values) {
+    console.log(values);
+    let {
+      nguoi_dung_id,
+      ten_nguoi_dung,
+      email,
+      pass_word,
+      phone,
+      birth_day,
+      gender,
+      role,
+      skill,
+      certification
+    } = values;
+    try {
+      let checkUser = await this.prisma.nguoiDung.findFirst({ where: { email} });
+
+      if (!checkUser) {
+
+        let updateUser = {
+          nguoi_dung_id,
+          ten_nguoi_dung,
+          email,
+          pass_word,
+          phone,
+          birth_day,
+          gender,
+          role,
+          skill,
+          certification
+        };
+
+        await this.prisma.nguoiDung.create({data: updateUser});
+
+        return {...updateUser, message: "Tạo người dùng thành công"};
+      } else {
+        throw new HttpException({ content: "email đã tồn tại", code: 404}, 404);
+        // return "Tạo thất bại"
+      }
+
+    } catch (error) {
+      console.log(error);
+      throw new HttpException(error.response.content, error.status);
+      // return "Lỗi BE"
+    }
+
 
   }
 
