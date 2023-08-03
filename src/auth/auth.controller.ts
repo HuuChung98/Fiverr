ab97c12f4ce535daccae3215b9dcd7be89ebbca3
HuuChung98 <balechung93@gmail.com>
2023-08-03 17:15:53 +0700

    create API login and register

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,27 +8,12 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
+  login(@Body() body) {
+    return this.authService.login(body);
   }
 
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  @Post()
+  register(@Body() body) {
+    return this.authService.register(body);
   }
 }
