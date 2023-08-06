4f8fecd110fe2badd60be56eb0f8fbdb0ee357fe
HuuChung98 <balechung93@gmail.com>
2023-08-06 22:08:57 +0700

    completed API skill

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { SkillService } from './skill.service';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
-import { ApiParam, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiHeader, ApiParam, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
 
 @ApiTags("Skill")
 @Controller('api/skill')
 export class SkillController {
   constructor(private readonly skillService: SkillService) {}
 
+  @ApiBearerAuth()
+@ApiHeader({ name: "Token", description: "JWT token" })
+@UseGuards(AuthGuard("jwt")) // jwt là key mặc định
   @Get()
   getSkill() {
     return this.skillService.getSkill();
--- src/skill/skill.service.ts
+++ src/skill/skill.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSkillDto } from './dto/create-skill.dto';
 import { UpdateSkillDto } from './dto/update-skill.dto';
+import { NguoiDung, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class SkillService {
 
-  getSkill() {
-    return `This action returns all skill`;
+  prisma = new PrismaClient();
+
+  async getSkill() {
+    let userList = await this.prisma.nguoiDung.findMany();
+    
+    let skillList = new Array;
+    userList.map((item) => skillList.push(item?.skill))
+    return skillList.sort().join(", ").toString();
   }
 }
