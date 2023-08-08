9aca0cbec2857a75f281f8e0cf2cef89d2630550
HuuChung98 <balechung93@gmail.com>
2023-08-08 13:22:49 +0700

    complete API LoaiCongViec

--- src/job-type/job-type.controller.ts
+++ src/job-type/job-type.controller.ts
@@ -1,39 +1,55 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { JobTypeService } from './job-type.service';
 import { CreateJobTypeDto } from './dto/create-job-type.dto';
 import { UpdateJobTypeDto } from './dto/update-job-type.dto';
 import { ApiParam, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
+
+class JobType  {
+  @ApiProperty({ description: "loaiCongViec_id", type: Number})
+  loaiCongViec_id : number;
+
+  @ApiProperty( { description: "tenLoaiCongViec", type: String})
+  ten_loai_cong_viec: string;
+}
+
 
 @ApiTags("LoaiCongViec") // gom nhóm API trong swagger
 @Controller('api/loai-cong-viec')
 export class JobTypeController {
   constructor(private readonly jobTypeService: JobTypeService) {}
 
+  // Tạo loại công viêc
   @Post()
-  createJobType(@Body() createJobTypeDto: CreateJobTypeDto) {
-    return this.jobTypeService.createJobType(createJobTypeDto);
+  createJobType(@Body() body: JobType) {
+    return this.jobTypeService.createJobType(body);
   }
 
+  // Lấy về tất cả loại công việc
   @Get()
   getJobType() {
     return this.jobTypeService.getJobType();
   }
 
-  @Get()
-  getJobType_Page() {
-    return this.jobTypeService.getJobType_Page();
+  // Phân trang tìm kiếm loai công việc 
+  @Get("phan-trang-tim-kiem")
+  getJobType_Page(@Query("pageIndex") pageIndex: number, @Query("pageSize") pageSize: number, @Query("keyword") keyword: string) {
+    const paginationOptions = { pageIndex, pageSize }
+    return this.jobTypeService.getJobType_Page(paginationOptions, keyword);
   }
 
+  // Lấy thông tin loại công việc theo Id công việc
   @Get(':id')
   jobTypeDetail(@Param('id') id: string) {
     return this.jobTypeService.jobTypeDetail(+id);
   }
 
+  // Chỉnh sửa lại loại công việc
   @Patch(':id')
-  updateTypeJob(@Param('id') id: string) {
-    return this.jobTypeService.updateTypeJob(+id);
+  updateTypeJob(@Param('id') id: string, @Body() payload: JobType) {
+    return this.jobTypeService.updateTypeJob(+id, payload);
   }
-
+ 
+  // Xóa loại công việc đã tạo
   @Delete(':id')
   removeJobType(@Param('id') id: string) {
     return this.jobTypeService.removeJobType(+id);
--- src/job-type/job-type.service.ts
+++ src/job-type/job-type.service.ts
@@ -1,31 +1,68 @@
 import { Injectable } from '@nestjs/common';
 import { CreateJobTypeDto } from './dto/create-job-type.dto';
 import { UpdateJobTypeDto } from './dto/update-job-type.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class JobTypeService {
-  createJobType(createJobTypeDto: CreateJobTypeDto) {
-    return 'tra ve loai cong viec';
+
+  prisma = new PrismaClient();
+
+  async createJobType(body) {
+    let {ten_loai_cong_viec} = body;
+    let jobType = await this.prisma.loaiCongViec.findMany({ where: { ten_loai_cong_viec}});
+    
+    if(jobType.length == 0) {
+      return "Loại công việc đã tồn tại"
+    }
+    return await this.prisma.loaiCongViec.create({ data: body});
+     
   }
 
-  getJobType() {
-    return `Tra ve loai cong viec`;
-  }
-
-  getJobType_Page() {
-    return `Phan trang loai cong viec`;
-  }
-
-  jobTypeDetail(id: number) {
-    return `Tra ve chi tiet loai cong viec`;
-  }
-
-  updateTypeJob(id: number) {
-    return `Tra ve cap nhat loai cong viec`;
+  async getJobType() {
+    let jobType = await this.prisma.loaiCongViec.findMany();
+    return jobType
   }
 
 
-  removeJobType(id: number) {
-    return `Xoa loai cong viec`;
+  async getJobType_Page(paginationOptions, keyword) {
+    const { pageIndex, pageSize } = paginationOptions;
+    const skip = (pageIndex - 1) * pageSize;
+
+    let jobType = await this.prisma.loaiCongViec.findMany({ where: { ten_loai_cong_viec: keyword}, take: Number(pageSize), skip: skip});
+
+    return jobType;
+  }
+
+  async jobTypeDetail(id: number) {
+    let jobTypeDetail = await this.prisma.loaiCongViec.findFirst({where: {
+      loaiCongViec_id: id
+    }}) 
+    return jobTypeDetail;
+  }
+
+  async updateTypeJob(id: number, payload) {
+    let checkLoaiCongViec  = await this.prisma.loaiCongViec.findFirst({where: {
+      loaiCongViec_id: id
+    }});
+
+    if(checkLoaiCongViec) {
+      await this.prisma.loaiCongViec.update(payload)
+    }
+
+    return checkLoaiCongViec;
+  }
+
+
+  async removeJobType(id: number) {
+    let removeJobType = await this.prisma.loaiCongViec.findFirst({ where: {
+      loaiCongViec_id: id
+    }});
+    if(removeJobType) {
+      await this.prisma.loaiCongViec.delete({ where: {
+        loaiCongViec_id: id
+      }});
+      return `Đã xóa ${removeJobType.ten_loai_cong_viec}`;
+    }
   }
 }
