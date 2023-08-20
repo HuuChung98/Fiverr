1ab19fa7f78e4d6788458dfe75c75d14da048432
HuuChung98 <balechung93@gmail.com>
2023-08-20 12:57:57 +0700

    completed API Skill

--- src/skill/skill.controller.ts
+++ src/skill/skill.controller.ts
@@ -1,7 +1,5 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Headers } from '@nestjs/common';
 import { SkillService } from './skill.service';
-import { CreateSkillDto } from './dto/create-skill.dto';
-import { UpdateSkillDto } from './dto/update-skill.dto';
 import { ApiBearerAuth, ApiHeader, ApiParam, ApiProperty, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { AuthGuard } from '@nestjs/passport';
 
@@ -11,10 +9,9 @@ export class SkillController {
   constructor(private readonly skillService: SkillService) { }
 
   @ApiBearerAuth()
-  @ApiHeader({ name: "Token", description: "JWT token" })
   @UseGuards(AuthGuard("jwt")) // jwt là key mặc định
   @Get()
-  getSkill() {
-    return this.skillService.getSkill();
+  getSkill(@Headers("token") token: string) {
+    return this.skillService.getSkill(token);
   }
 }
--- src/skill/skill.service.ts
+++ src/skill/skill.service.ts
@@ -1,18 +1,27 @@
 import { Injectable } from '@nestjs/common';
-import { CreateSkillDto } from './dto/create-skill.dto';
-import { UpdateSkillDto } from './dto/update-skill.dto';
-import { NguoiDung, PrismaClient } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/auth.constants';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class SkillService {
+  constructor(private jwtService: JwtService){}
 
   prisma = new PrismaClient();
 
-  async getSkill() {
-    let userList = await this.prisma.nguoiDung.findMany();
+  async getSkill(token) {
+    try {
+      await this.jwtService.verifyAsync(token, {
+        secret: jwtConstants.secret
+      })
+      let userList = await this.prisma.nguoiDung.findMany();
 
-    let skillList = new Array;
-    userList.map((item) => skillList.push(item?.skill))
-    return skillList.sort().join(", ").toString();
+      let skillList = new Array;
+      userList.map((item) => skillList.push(item?.skill))
+      return skillList.sort().join(", ").toString();
+    } catch (error) {
+      return "Lỗi xác thực";
+    }
+
   }
 }
