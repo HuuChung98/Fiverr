6301985fd61391f3b6a4390f6019d314cbb61b24
HuuChung98 <balechung93@gmail.com>
2023-08-21 16:33:42 +0700

    user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -74,18 +74,20 @@ export class UserService {
   }
 
   async removeUser(token, id: number) {
-
+    try {
       await this.jwtService.verifyAsync(token, {
         secret: jwtConstants.secret
       })
-      let userRemove = await this.prisma.nguoiDung.findFirst({ where: { nguoi_dung_id: id } })
-      if (userRemove) {
+      let user = await this.prisma.nguoiDung.findFirst({ where: { nguoi_dung_id: id } })
+      if (user) {
         await this.prisma.nguoiDung.delete({ where: { nguoi_dung_id: id } })
         return "Xóa người dùng thành công";
       } else {
         return "Xóa người dùng thất bại, kiểm tra lại ID của người dùng";
       }
-
+    } catch (error) {
+      return "Lỗi xác thực"
+    }
   }
 
   async userInfo(token, id: number) {
